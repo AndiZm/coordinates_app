e7e23b9f1df76ba6dc9c3a6aea56faf880e5b009
AndiZm <34646647+AndiZm@users.noreply.github.com>
2025-01-22 19:38:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,34 +2,14 @@ import streamlit as st
 import random
 import geopandas as gpd
 from shapely.geometry import Point
+from src import *
 
-# Function to generate random GPS coordinates
-def generate_random_point_in_VGN(bavaria_boundary, min_lat, max_lat, min_lon, max_lon):
-    while True:
-        # Generate random latitude and longitude
-        latitude = random.uniform(min_lat, max_lat)
-        longitude = random.uniform(min_lon, max_lon)
-
-        # Create a Point object
-        random_point = Point(longitude, latitude)
-
-        # Check if the point is within the VGN boundary
-        if random_point.within(bavaria_boundary):
-            return latitude, longitude
-
-# Load GeoJSON file
-geojson_file = "vgn.geojson"
-landkreise_gdf = gpd.read_file(geojson_file)
-bavaria_boundary = landkreise_gdf.unary_union
-
-# Latitude and longitude bounds for VGN
-min_lat, max_lat = 48.8593, 50.5231
-min_lon, max_lon = 10.0400, 12.5505
+vgn = region("vgn.geojson")
 
 # Streamlit UI
 st.title("Random GPS Coordinate Generator for VGN")
 st.write("Click the button to generate a random GPS coordinate within the VGN region.")
 
 if st.button("Generate Random GPS"):
-    lat, lon = generate_random_point_in_VGN(bavaria_boundary, min_lat, max_lat, min_lon, max_lon)
+    lat, lon = vgn.generate_random_point()
     st.success(f"{lat:.4f}, {lon:.4f}")
