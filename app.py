44d5f96fa5cc06456730e95428e2da7e23803d0b
AndiZm <34646647+AndiZm@users.noreply.github.com>
2025-01-22 19:42:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ from src import *
 vgn = region("vgn.geojson")
 
 # Streamlit UI
-st.title("Random GPS Coordinate Generator for VGN")
-st.write("Click the button to generate a random GPS coordinate within the VGN region.")
+st.title("Random GPS Coordinate Generator")
+st.write("Click the button to generate a random GPS coordinate within the region you prefer.")
 
-if st.button("Generate Random GPS (VGN)"):
+if st.button("Generate Point (VGN)"):
     lat, lon = vgn.generate_random_point()
     st.success(f"{lat:.4f}, {lon:.4f}")
