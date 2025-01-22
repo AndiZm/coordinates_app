dc481887da26b34fe0307ca003caa74e7f999a47
AndiZm <34646647+AndiZm@users.noreply.github.com>
2025-01-22 19:53:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from shapely.geometry import Point
 from src import *
 
 vgn = region("vgn.geojson")
-#bayern = region("bayern.geojson")
+bayern = region("bayern.geojson")
 
 # Streamlit UI
 st.title("Random GPS Coordinate Generator")
@@ -15,6 +15,6 @@ if st.button("VGN"):
     lat, lon = vgn.generate_random_point()
     st.success(f"{lat:.4f}, {lon:.4f}")
 
-#if st.button("Bavaria"):
-#    lat, lon = bayern.generate_random_point()
-#    st.success(f"{lat:.4f}, {lon:.4f}")
+if st.button("Bavaria"):
+    lat, lon = bayern.generate_random_point()
+    st.success(f"{lat:.4f}, {lon:.4f}")
