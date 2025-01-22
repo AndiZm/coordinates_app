11a6a4e824e9313bfd8ddde3d476eac0ff5dcb03
AndiZm <34646647+AndiZm@users.noreply.github.com>
2025-01-22 19:49:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,16 @@ from shapely.geometry import Point
 from src import *
 
 vgn = region("vgn.geojson")
+bayern = region("bayern.geojson")
 
 # Streamlit UI
 st.title("Random GPS Coordinate Generator")
 st.write("Click the button to generate a random GPS coordinate within the region you prefer.")
 
-if st.button("Generate Point (VGN)"):
+if st.button("VGN"):
     lat, lon = vgn.generate_random_point()
+    st.success(f"{lat:.4f}, {lon:.4f}")
+
+if st.button("Bavaria"):
+    lat, lon = bayern.generate_random_point()
     st.success(f"{lat:.4f}, {lon:.4f}")
