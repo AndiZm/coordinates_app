277038e754b3097e87ae2affb21623914d83d7c2
AndiZm <34646647+AndiZm@users.noreply.github.com>
2025-01-22 19:51:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,6 @@ if st.button("VGN"):
     lat, lon = vgn.generate_random_point()
     st.success(f"{lat:.4f}, {lon:.4f}")
 
-if st.button("Bavaria"):
-    lat, lon = bayern.generate_random_point()
-    st.success(f"{lat:.4f}, {lon:.4f}")
+#if st.button("Bavaria"):
+#    lat, lon = bayern.generate_random_point()
+#    st.success(f"{lat:.4f}, {lon:.4f}")
